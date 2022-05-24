485f669b7aa568b2434a4e05473b7cfffcca271f
Julien Bernard <jbernard@c2r4p6.42quebec.com>
2022-05-24 15:26:33 -0400

    :construction: Working on envp_utils

--- src/envp/envp_utils.c
+++ src/envp/envp_utils.c
@@ -9,17 +9,26 @@ void	print_envp(char **envp, char *header)
 		printf("%s\n", envp[i]);
 		i++;
 	}
-	printf("************ Line count (i) : %d ***************\n", i);
+	printf("************ Line count (i) : %d ***************\n\n", i);
 }
 
 void	envp_utils_test(t_mnshl *vars)
 {
+	print_envp(vars->envp, "AT BEGINNING");
 	printf("_ : %s\n", envp_get_value_line(vars, "_"));
 	printf("ITER : %s\n", envp_get_value_line(vars, "ITER"));
 	printf("HOMEBREW : %s\n", envp_get_value_line(vars, "HOMEBREW"));
 	printf("HOMEBREW_CACHE : %s\n", envp_get_value_line(vars, "HOMEBREW_CACHE"));
 	printf("USER : %s\n", envp_get_value_line(vars, "USER"));
+	printf("USER : %s\n", envp_get_value_line(vars, "USER"));
+	envp_remove_line(vars, "ITER");
+	envp_remove_line(vars, "_");
+	envp_remove_line(vars, "USER");
+	envp_remove_line(vars, "HOMEBREW");
+	print_envp(vars->envp, "AFTER DELETION");
 }
+
+/* _____________________________________________________________________________________________*/
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -37,6 +46,43 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	return (NULL);
 }
 
+
+int		is_name_in_line(char *envline, char *name)
+{
+	int	len;
+
+	len = ft_strlen(name);
+	if (!ft_strncmp(envline, name, len) && envline[len] == '=')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int		is_name_in_envp(char **envp, char *name)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (is_name_in_line(envp[i], name))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	*build_envp_line(char *name, char *value)
+{
+	char *line;
+
+	line = ft_strjoin(name, "=");
+	line = ft_strjoin(line, value);
+
+	return (line);
+}
+
 /* _____________________________________________________________________________________________*/
 
 /* Remove line from vars.envp
@@ -52,24 +98,31 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 void envp_remove_line(t_mnshl *vars, char *name)
 {
 	int i;
-	int deleted;
-	deleted = 0;
-	i = 0;
-	while (vars->envp[i])
+	int j;
+	char **n_envp;
+	
+	if (is_name_in_envp(vars->envp, name) >= 0)
 	{
-		if (!ft_strncmp(vars->envp[i], name, ft_strlen(name)) && !deleted)
-		{	
-			if (vars->envp[i][ft_strlen(name)] == '=')
-				deleted = 1;
-		}
-		if (deleted)
+		n_envp = malloc(sizeof(char *) * (ft_strtablen(vars->envp) - 1));
+		if (!n_envp)
+			return ;
+		i = 0;
+		j = 0;
+		while (vars->envp[i])
 		{
-			if (vars->envp[i + 1])
-				vars->envp[i] = ft_strdup(vars->envp[i + 1]);
-			else 
-				vars->envp[i] = NULL;
+			if (!is_name_in_line(vars->envp[i], name))
+			{
+				n_envp[j] = vars->envp[i];
+				i++;
+				j++;
+			}
+			else
+			{
+				i++;
+			}
 		}
-		i++;
+		ft_freetabstr(&vars->envp);
+		vars->envp = n_envp;
 	}
 }
 
@@ -86,7 +139,33 @@ void envp_remove_line(t_mnshl *vars, char *name)
 // TODO check is case sensitive
 void envp_set_line(t_mnshl *vars, char *name, char *value)
 {
-	//if no exists -> append
+	int		i;
+	char	*n_line;
+	char 	**n_envp;
+
+	n_line = build_envp_line(name, value);
+	i = is_name_in_envp(vars->envp, name);
+	if (i >= 0)
+	{
+		free(vars->envp[i]);
+		vars->envp[i] = n_line;
+	}
+	if (i < 0)
+	{
+		i = 0;
+		n_envp = malloc(sizeof(char *) * (ft_strtablen(vars->envp) + 2));
+		if (!n_envp)
+			return ;
+		while (vars->envp[i])
+		{
+			n_envp[i] = vars->envp[i];
+			i++;
+		}
+		n_envp[i++] = n_line;
+		n_envp[i] = "";
+		ft_freetabstr(vars->envp);
+		vars->envp = n_envp;
+	}
 }
 
 /* Set line in envp from main structure of program. If not exists the
@@ -107,17 +186,14 @@ char *envp_get_value_line(t_mnshl *vars, char *name)
 	char 	*ret;
 	int 	i;
 
-	i = 0;
 	ret = NULL;
+	i = 0;
 	while (vars->envp[i])
 	{
-		if (!ft_strncmp(vars->envp[i], name, ft_strlen(name)))
+		if (is_name_in_line(vars->envp[i], name) >= 0)
 		{	
-			if (vars->envp[i][ft_strlen(name)] == '=')
-			{
-				ret = &vars->envp[i][ft_strlen(name) + 1];
-				break ;
-			}
+			ret = &vars->envp[i][ft_strlen(name) + 1];
+			return (ret);
 		}
 		i++;
 	}
