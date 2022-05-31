e82d936e9f4b07fa31e9a31502895fe3e1c9a88e
Julien Bernard <jbernard@c2r4p6.42quebec.com>
2022-05-31 12:57:52 -0400

    Moved put_envp to put_utils.c, made it available in header files

--- test/test_envp_utils.c
+++ test/test_envp_utils.c
@@ -1,21 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_envp_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbernard <jbernard@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 12:51:18 by jbernard          #+#    #+#             */
+/*   Updated: 2022/05/31 12:51:35 by jbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "minishell.h"
+#include "lib_test_header.h"
 
-void	test_envp_utils(t_mnshl *vars)
+void	envp_remove_line(char **envp, char *name);
+void	envp_set_line(char **envp, char *name, char *value);
+char	*envp_get_value_line(char **envp, char *name);
+void	test_envp_utils(t_test *count)
 {
-	print_envp(vars->envp, "AT BEGINNING");
-	printf("_ : %s\n", envp_get_value_line(vars->envp, "_"));
-	printf("ITER : %s\n", envp_get_value_line(vars->envp, "ITER"));
-	printf("HOMEBREW : %s\n", envp_get_value_line(vars->envp, "HOMEBREW"));
-	printf("HOMEBREW_CACHE : %s\n", envp_get_value_line(vars->envp, "HOMEBREW_CACHE"));
-	printf("USER : %s\n", envp_get_value_line(vars->envp, "USER"));
-	printf("USER : %s\n", envp_get_value_line(vars->envp, "USER"));
-	// envp_remove_line(vars->envp, "ITER");
-	envp_remove_line(vars->envp, "_");
-	// envp_remove_line(vars->envp, "USER");
-	// envp_remove_line(vars->envp, "HOMEBREW");
-	envp_set_line(vars->envp, "MYVAR", "MYVALUE");
-	envp_set_line(vars->envp, "MYOTHERVAR", "MYOTHERVALUE");
-	envp_set_line(vars->envp, "MYVAR", "MYOTHEROTHERVALUE");
-	print_envp(vars->envp, "AFTER DELETION");
+	T_FIRST_LVL;
 }
